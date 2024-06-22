61a96e9375ca0b4b8f221a410f36b9ebc0a8f92d
Tomáš Děd <tomas-ded@seznam.cz>
2024-06-22 11:27:27 +0200

    major typing refactor

--- algpy_src/algorithms/algorithm.py
+++ algpy_src/algorithms/algorithm.py
@@ -1,22 +1,27 @@
-import numpy as np
 import time
-from typing import Optional, Iterable, TypeVar, Generic
 from abc import abstractmethod
+from typing import Optional, Iterable, TypeVar, Generic, Any
+
+import numpy as np
+
 from algpy_src.base.complexity_object import ComplexityObject
-from algpy_src.base.constants import COMPLEXITIES
+from algpy_src.base.constants import COMPLEXITIES, PrintableComparable
 
-AlgInstance = TypeVar('AlgInstance')
+ProblemInstance = TypeVar('ProblemInstance')
+InputSize = TypeVar('InputSize', bound=PrintableComparable)
 
 
-class Algorithm(ComplexityObject, Generic[AlgInstance]):
+class Algorithm(ComplexityObject, Generic[ProblemInstance, InputSize]):
     """
     Base class for all algorithms.
+    Inheriting class should specify type hints for ProblemInstance and InputSize
     """
 
     def __init__(self):
         super().__init__()
 
     @property
+    @abstractmethod
     def name(self) -> str:
         return 'Generic Algorithm Class'
 
@@ -36,7 +41,7 @@ class Algorithm(ComplexityObject, Generic[AlgInstance]):
 
         Parameters:
         ----------
-        which : str
+        which : str (default 'both')
             Select the type of complexity measure you want to print (one of 'both', 'time' or 'space').
         """
         if which == 'time' or which == 'both':
@@ -57,65 +62,82 @@ class Algorithm(ComplexityObject, Generic[AlgInstance]):
         print(f'Space complexity of the {self.name} algorithm is O({self.space_complexity})')
 
     @abstractmethod
-    def generate_increasing_input_size_sequence(self) -> Iterable[tuple[int, ...]]:
+    def generate_increasing_input_size_sequence(self, *args: Any, **kwargs: Any) -> Iterable[InputSize]:
         """
+        Parameters
+        ----------
+        *args : Any
+            Additional arguments passed to the input sizes generating function.
+        **kwargs : Any
+            Additional keyword arguments passed to the input sizes generating function.
 
         Returns
         -------
-        input_size_sequence : Iterable[tuple[int, ...]]
-            Sequence of viable input size parameters for generate_random_input(input_size) in increasing order
+        input_size_sequence : Iterable[InputSize]
+            Sequence of viable input size parameters for generate_random_input(input_size) in order of increasing complexity.
         """
         raise NotImplementedError()
 
     @abstractmethod
-    def generate_random_input(self, input_size: tuple[int, ...]) -> AlgInstance:
+    def generate_random_input(self, input_size: InputSize, seed: Optional[int] = None, *args: Any, **kwargs: Any) -> ProblemInstance:
         """
         Way to generate single random input instance of given size.
         Output of this function has to be accepted by run_algorithm().
 
         Parameters
         ----------
-        input_size : tuple[int, ...]
-            Desired input size (may be composed of multiple integers, e.g., for matrices).
+        input_size : InputSize
+            Desired input size (form depends on specific algorithm).
+        seed : Optional[int] (default None)
+            Seed for generating random instance.
+        *args : Any
+            Additional arguments passed to the generating function.
+        **kwargs : Any
+            Additional keyword arguments passed to the generating function.
 
         Returns
         -------
-        instance : AlgInstance
+        instance : ProblemInstance
             A problem instance supported in run_algorithm(input_instance=instance).
         """
         raise NotImplementedError()
 
     @abstractmethod
-    def run_algorithm(self, input_instance: AlgInstance, *args, **kwargs) -> tuple[Optional[AlgInstance], int]:
+    def run_algorithm(self, input_instance: ProblemInstance, *args: Any, **kwargs: Any) -> tuple[Optional[ProblemInstance], int]:
         """
         The main run function of each algorithm. The algorithms should be able to internally count number of ops.
 
         Parameters
         ----------
-        input_instance : AlgInstance
+        input_instance : ProblemInstance
             Instance on which to run the algorithm.
-        *args
+        *args : Any
             Additional arguments passed to the algorithm.
-        **kwargs
+        **kwargs : Any
             Additional keyword arguments passed to the algorithm.
 
         Returns
         -------
-        output_instance : Optional[AlgInstance]
+        output_instance : Optional[ProblemInstance]
             Returns input processed by the algorithm if relevant.
         number_of_operations : int
             Outputs the total number of operations made by the algorithm.
         """
         raise NotImplementedError()
 
-    def analyse_runtime(self, n: int = 10, *args, **kwargs) -> None:
+    def analyse_runtime(self, input_sequence: Optional[dict[InputSize, ProblemInstance]], seed: Optional[int] = None, n: int = 10, *args, **kwargs) -> None:
         """
-        Runs algorithm on randomly generated instances of given sizes
+        Runs algorithm on randomly generated instances of given sizes.
 
         Parameters
         ----------
-        n : int
-            Number of repetitions per input size for the statistical analysis of algorithm's runtime
+        input_sequence : Optional[dict[InputSize, ProblemInstance]]
+            Optional dictionary of input size : problem instance pairs to analyse the algorithm runtime on.
+            If not given, random inputs are generated.
+        seed : Optional[int] (default None)
+            Seed for generating random instances in case input_sequence not provided.
+        n : int (default 10)
+            Number of repetitions per input size for the statistical analysis of algorithm's runtime.
         *args
             Arguments passed to the run_algorithm() function call.
         **kwargs
@@ -125,19 +147,21 @@ class Algorithm(ComplexityObject, Generic[AlgInstance]):
         -------
 
         """
-        input_sizes = self.generate_increasing_input_size_sequence()
+        if input_sequence is None:
+            input_sizes: Iterable[InputSize] = self.generate_increasing_input_size_sequence()
+            input_sequence = {input_size: self.generate_random_input(input_size, seed, args, kwargs) for input_size in input_sizes}
+
         print('-' * 10)
         self.print_time_complexity_info()
         print(f'Analysing runtime of the {self.name} algorithm:')
-        for input_size in input_sizes:
+        for input_size, problem_instance in input_sequence.items():
 
             runtimes: list[float] = []
             ops_counts: list[int] = []
 
             for _ in range(n):
-                input_instance = self.generate_random_input(input_size)
                 start = time.time()
-                run_output = self.run_algorithm(input_instance, args, kwargs)
+                run_output = self.run_algorithm(problem_instance, args, kwargs)
                 runtimes.append(time.time() - start)
                 ops_counts.append(run_output[1])
 
@@ -145,5 +169,5 @@ class Algorithm(ComplexityObject, Generic[AlgInstance]):
             std_secs = np.std(runtimes)
             avg_ops = np.mean(ops_counts)
             std_ops = np.std(ops_counts)
-            print(f'\tRun with input size {input_size} took {avg_secs:_.2f}', u'\u00B1', f'{std_secs:_2f} seconds',
-                  f'and {avg_ops:_.2f}', u'\u00B1', f'{std_ops:_2f} operations ({n=} samples)')
+            print(f'\tRun on instance with problem size {input_size} took {avg_secs:_.2f}', u'\u00B1', f'{std_secs:_2f} seconds',
+                  f'and {avg_ops:_.2f}', u'\u00B1', f'{std_ops:_2f} operations ({n=} repetitions)')
--- algpy_src/algorithms/sorting/bubble_sort.py
+++ algpy_src/algorithms/sorting/bubble_sort.py
@@ -1,11 +1,11 @@
 import random
-from typing import Iterable, Optional
+from typing import Iterable, Optional, Any
 
 from algpy_src.algorithms.algorithm import Algorithm
 from algpy_src.base.constants import Comparable
 
 
-class BubbleSort(Algorithm):
+class BubbleSort(Algorithm[Iterable[Comparable], int]):
 
     def __init__(self):
         super().__init__()
@@ -22,12 +22,46 @@ class BubbleSort(Algorithm):
     def space_complexity(self) -> str:
         return '1'
 
-    def generate_increasing_input_size_sequence(self) -> Iterable[tuple[int, ...]]:
-        return [10 ** i for i in range(0, 7)]
+    def generate_increasing_input_size_sequence(self, n: int = 6, *args: Any, **kwargs: Any) -> list[int]:
+        """
+        Generates a list of increasing integers corresponding to number of elements in the to-be-sorted sequence.
 
-    def generate_random_input(self, input_size: tuple[int, ...]) -> Iterable[Comparable]:
-        return [random.randint(1, 1_000) for _ in range(input_size[0])]
+        Parameters
+        ----------
+        n : int
+            Exponent of the highest number in the list (10^n).
+        *args : Any
+            Additional arguments passed to the input sizes generating function.
+        **kwargs : Any
+            Additional keyword arguments passed to the input sizes generating function.
 
-    def run_algorithm(self, input_instance: Iterable[Comparable], *args, **kwargs) -> tuple[Optional[Iterable[Comparable]], int]:
+        Returns
+        -------
+        input_sizes : list[int]
+            List of increasing integers specified as exponents of 10 from 10^0 up to 10^n.
+        """
+        return [10 ** i for i in range(n)]
+
+    def generate_random_input(self, input_size: int, seed: Optional[int] = None, *args: Any, **kwargs: Any) -> list[int]:
+        """
+        Generates a list of random integers from range 1 to input_size with replacement.
+
+        Parameters
+        ----------
+        input_size : int
+            Length of the generated list.
+        seed : Optional[int]
+            Seed for the random generation.
+        args
+        kwargs
+
+        Returns
+        -------
+
+        """
+        rng = random.Random(seed)
+        return [rng.randint(1, input_size) for _ in range(input_size)]
+
+    def run_algorithm(self, input_instance: Iterable[Comparable], *args, **kwargs) -> tuple[Iterable[Comparable], int]:
         # TODO
         return input_instance, 0
