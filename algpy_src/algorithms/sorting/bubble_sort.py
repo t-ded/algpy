3d350b7555381efedc0307e8d326cd33179610f0
Tomáš Děd <tomas-ded@seznam.cz>
2024-06-23 10:19:05 +0200

    implement bubble sort

--- algpy_src/algorithms/sorting/bubble_sort.py
+++ algpy_src/algorithms/sorting/bubble_sort.py
@@ -52,9 +52,10 @@ class BubbleSort(Algorithm[Iterable[Comparable], int]):
             Length of the generated list.
         seed : Optional[int]
             Seed for the random generation.
-        args
-        kwargs
-
+        args : Any
+            Additional arguments passed to the random instance generating function.
+        kwargs : Any
+            Additional keyword arguments passed to the random instance generating function.
         Returns
         -------
 
@@ -62,6 +63,35 @@ class BubbleSort(Algorithm[Iterable[Comparable], int]):
         rng = random.Random(seed)
         return [rng.randint(1, input_size) for _ in range(input_size)]
 
-    def run_algorithm(self, input_instance: Iterable[Comparable], *args, **kwargs) -> tuple[list[Comparable], int]:
-        # TODO
-        return [], 0
+    def run_algorithm(self, input_instance: Iterable[Comparable], descending: bool = True, *args: Any, **kwargs: Any) -> tuple[list[Comparable], int]:
+        """
+        Run function of the bubble sort algorithm implemented in a stable (relative order of same-valued keys remains) manner.
+        Implementation assumes the less naive approach, taking into account that bubbled elements at the end of the array are known to be sorted already.
+
+        Parameters
+        ----------
+        input_instance : Iterable[Comparable]
+            Iterable input instance of comparable objects to run the sorting algorithm on.
+            It is copied and then returned as a sorted list.
+        descending : bool (default True)
+            Whether to sort in descending order.
+            Note that the algorithm is built in a stable manner, so that the relative order of items with equal value remains intact.
+        args : Any
+            Additional arguments passed to the running function.
+        kwargs : Any
+            Additional keyword arguments passed to the running function.
+        Returns
+        -------
+        input_list : list[Comparable]
+            List copy of the given input instance iterable sorted in the required order.
+        n_ops : int
+            Number of operations (element switches) that were required.
+        """
+        n_ops = 0
+        input_list = list(input_instance)
+        for i in range(len(input_list)):
+            for j in range(len(input_list) - i - 1):
+                if (descending is True and input_list[j] < input_list[j + 1]) or (descending is False and input_list[j] > input_list[j + 1]):
+                    input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
+                    n_ops += 1
+        return input_list, n_ops
