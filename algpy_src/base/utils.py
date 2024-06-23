b9ea0d652d9f91897e07ff944f6e7e7cdca0e6d2
Tomáš Děd <tomas-ded@seznam.cz>
2024-06-23 10:54:08 +0200

    add printing utility functions

--- algpy_src/base/utils.py
+++ algpy_src/base/utils.py
@@ -1,1 +1,24 @@
+def print_delimiter(delimiter: str = '-', n: int = 10) -> None:
+    """
+    Prints the given delimiter for n times
 
+    Parameters
+    ----------
+    delimiter : str (default '-')
+        Delimiter to print.
+    n : int (default 10)
+        Number of delimiter's repetitions.
+    """
+    print(delimiter * n)
+
+    
+def print_gap(n: int = 3) -> None:
+    """
+    Prints n empty rows.
+
+    Parameters
+    ----------
+    n : int (default 3)
+        Number of rows to print.
+    """
+    print_delimiter('\n', n)
