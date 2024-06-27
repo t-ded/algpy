73c7741fe9105493a80d88d6d112848af5125e43
Tomáš Děd <tomas-ded@seznam.cz>
2024-06-27 15:58:11 +0200

    make example complexity object implement all abstractmethods

--- algpy_src/tests/test_utils/base_objects.py
+++ algpy_src/tests/test_utils/base_objects.py
@@ -6,3 +6,15 @@ class ExampleComplexityObject(ComplexityObject):
     @property
     def name(self) -> str:
         return 'Example Complexity Object'
+
+    def print_time_complexity_info(self) -> None:
+        """
+        Print the time complexity information of the example complexity object.
+        """
+        raise NotImplementedError()
+
+    def print_space_complexity_info(self) -> None:
+        """
+        Print the space complexity information of the example complexity object.
+        """
+        raise NotImplementedError()
