c973bce7b00df79c9b68480a7aca7d2ecc1211e1
Tomáš Děd <tomas-ded@seznam.cz>
2024-07-02 08:25:01 +0200

    add n_ops tests for base complexity obj

--- algpy_src/tests/base/test_complexity_object.py
+++ algpy_src/tests/base/test_complexity_object.py
@@ -4,3 +4,6 @@ from algpy_src.tests.test_utils.base_objects import ExampleComplexityObject
 def test_complexity_object():
     obj = ExampleComplexityObject()
     assert obj.name == 'Example Complexity Object'
+    assert obj.n_ops == 0
+    obj.increment_n_ops(5)
+    assert obj.n_ops == 5
