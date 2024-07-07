c357e45ba359a455cd2d2021772d7358b5e0790c
Tomáš Děd <tomas-ded@seznam.cz>
2024-07-07 18:30:45 +0200

    implement example testing algorithm

--- algpy_src/tests/test_utils/base_objects.py
+++ algpy_src/tests/test_utils/base_objects.py
@@ -3,7 +3,7 @@ from typing import Any, Optional, Iterable
 from algpy_src.algorithms.algorithm import Algorithm
 from algpy_src.algorithms.sorting.sorting_algorithm import SortingAlgorithm
 from algpy_src.base.complexity_object import ComplexityObject
-from algpy_src.base.constants import Comparable, VERBOSITY_LEVELS
+from algpy_src.base.constants import Comparable, VERBOSITY_LEVELS, InputSize
 
 
 class ExampleComplexityObject(ComplexityObject):
@@ -15,6 +15,47 @@ class ExampleComplexityObject(ComplexityObject):
     @property
     def space_complexity(self) -> str:
         return 'N/A'
+
+
+class ExampleAlgorithm(Algorithm[Iterable[Comparable], int]):
+
+    @property
+    def name(self) -> str:
+        return 'Example Algorithm'
+
+    @property
+    def best_case_time_complexity(self) -> str:
+        return 'N/A'
+
+    @property
+    def best_case_description(self) -> str:
+        return 'N/A'
+
+    @property
+    def average_case_time_complexity(self) -> str:
+        return 'N/A'
+
+    @property
+    def worst_case_time_complexity(self) -> str:
+        return 'N/A'
+
+    @property
+    def worst_case_description(self) -> str:
+        return 'N/A'
+
+    @property
+    def space_complexity(self) -> str:
+        return 'N/A'
+
+    def generate_increasing_input_size_sequence(self, *args: Any, **kwargs: Any) -> Iterable[int]:
+        return [1]
+
+    def generate_worst_case(self, input_size: InputSize, *args: Any, **kwargs: Any) -> Iterable[Comparable]:
+        return [1]
+
+    def run_algorithm(self, input_instance: Iterable[Comparable], verbosity_level: VERBOSITY_LEVELS = 0,
+                      *args: Any, **kwargs: Any) -> Optional[Iterable[Comparable]]:
+        return None
 
 
 class ExampleSortingAlgorithm(SortingAlgorithm, Algorithm[Iterable[Comparable], int]):
@@ -48,9 +89,6 @@ class ExampleSortingAlgorithm(SortingAlgorithm, Algorithm[Iterable[Comparable], 
         return 'N/A'
 
     def generate_increasing_input_size_sequence(self, *args: Any, **kwargs: Any) -> Iterable[int]:
-        return [1, 2, 3]
-
-    def generate_random_input(self, input_size: int, seed: Optional[int] = None, *args: Any, **kwargs: Any) -> Iterable[Comparable]:
         return [1, 2, 3]
 
     def generate_worst_case(self, input_size: int, *args: Any, **kwargs: Any) -> Iterable[Comparable]:
