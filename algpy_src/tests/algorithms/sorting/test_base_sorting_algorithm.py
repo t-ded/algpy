1f7bd2e194ffaf3a0e7efa56fe7f71cde2ed9847
Tomáš Děd <tomas-ded@seznam.cz>
2024-07-07 09:25:32 +0200

    add tests for base sorting algorithm

---
+++ algpy_src/tests/algorithms/sorting/test_base_sorting_algorithm.py
@@ -0,0 +1,30 @@
+import pytest
+
+from algpy_src.tests.test_utils.base_objects import ExampleSortingAlgorithm
+
+
+@pytest.fixture()
+def sa() -> ExampleSortingAlgorithm:
+    return ExampleSortingAlgorithm()
+
+
+def test_base_sorting_algorithm(sa: ExampleSortingAlgorithm) -> None:
+    assert sa.name == 'Example Sorting Algorithm'
+
+    assert sa.n_comparisons == 0
+    sa.increment_n_comparisons(5)
+    assert sa.n_comparisons == 5
+    sa.reset_n_comparisons()
+    assert sa.n_comparisons == 0
+    sa.increment_n_comparisons(1)
+
+    assert sa.n_swaps == 0
+    sa.increment_n_swaps(3)
+    assert sa.n_swaps == 3
+    sa.reset_n_swaps()
+    assert sa.n_swaps == 0
+    sa.increment_n_swaps(1)
+
+    sa.reset_all_counters()
+    assert sa.n_comparisons == 0
+    assert sa.n_swaps == 0
--- algpy_src/tests/test_utils/base_objects.py
+++ algpy_src/tests/test_utils/base_objects.py
@@ -1,4 +1,9 @@
+from typing import Any, Optional, Iterable
+
+from algpy_src.algorithms.algorithm import Algorithm
+from algpy_src.algorithms.sorting.base_sorting_algorithm import SortingAlgorithm
 from algpy_src.base.complexity_object import ComplexityObject
+from algpy_src.base.constants import Comparable, VERBOSITY_LEVELS
 
 
 class ExampleComplexityObject(ComplexityObject):
@@ -10,3 +15,47 @@ class ExampleComplexityObject(ComplexityObject):
     @property
     def space_complexity(self) -> str:
         return 'N/A'
+
+
+class ExampleSortingAlgorithm(SortingAlgorithm, Algorithm[Iterable[Comparable], int]):
+
+    @property
+    def name(self) -> str:
+        return 'Example Sorting Algorithm'
+
+    @property
+    def best_case_time_complexity(self) -> str:
+        return 'N/A'
+
+    @property
+    def best_case_description(self) -> str:
+        return 'N/A'
+
+    @property
+    def average_case_time_complexity(self) -> str:
+        return 'N/A'
+
+    @property
+    def worst_case_time_complexity(self) -> str:
+        return 'N/A'
+
+    @property
+    def worst_case_description(self) -> str:
+        return 'N/A'
+
+    @property
+    def space_complexity(self) -> str:
+        return 'N/A'
+
+    def generate_increasing_input_size_sequence(self, *args: Any, **kwargs: Any) -> Iterable[int]:
+        return [1, 2, 3]
+
+    def generate_random_input(self, input_size: int, seed: Optional[int] = None, *args: Any, **kwargs: Any) -> Iterable[Comparable]:
+        return [1, 2, 3]
+
+    def generate_worst_case(self, input_size: int, *args: Any, **kwargs: Any) -> Iterable[Comparable]:
+        return [1, 2, 3]
+
+    def run_algorithm(self, input_instance: Iterable[Comparable], verbosity_level: VERBOSITY_LEVELS = 0, descending: bool = True,
+                      *args: Any, **kwargs: Any) -> list[Comparable]:
+        return list(sorted(input_instance))
