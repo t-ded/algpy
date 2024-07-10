7894bcec378c81c585303823d11c7ea2db988ed4
Tomáš Děd <tomas-ded@seznam.cz>
2024-07-10 08:23:25 +0200

    add rest of linked list base tests and also add fr dll

--- algpy_src/tests/data_structures/linear/test_linked_list.py
+++ algpy_src/tests/data_structures/linear/test_linked_list.py
@@ -29,4 +29,36 @@ def test_singly_linked_list_base(sll):
     assert sll.worst_case_insert_time_complexity == 'n'
     assert sll.worst_case_insert_description == 'index equals length'
 
+    assert sll.best_case_search_time_complexity == '1'
+    assert sll.best_case_search_description == 'item in head'
+    assert sll.average_case_search_time_complexity == 'n'
+    assert sll.worst_case_search_time_complexity == 'n'
+    assert sll.worst_case_search_description == 'item not present'
 
+    assert sll.best_case_delete_time_complexity == '1'
+    assert sll.best_case_delete_description == 'item in head'
+    assert sll.average_case_delete_time_complexity == 'n'
+    assert sll.worst_case_delete_time_complexity == 'n'
+    assert sll.worst_case_delete_description == 'item at tail'
+
+
+def test_doubly_linked_list_base(dll):
+    assert dll.name == 'Doubly Linked List'
+
+    assert dll.best_case_insert_time_complexity == '1'
+    assert dll.best_case_insert_description == 'index 0 or index -1'
+    assert dll.average_case_insert_time_complexity == 'n'
+    assert dll.worst_case_insert_time_complexity == 'n'
+    assert dll.worst_case_insert_description == 'index equals half of length'
+
+    assert dll.best_case_search_time_complexity == '1'
+    assert dll.best_case_search_description == 'item in head'
+    assert dll.average_case_search_time_complexity == 'n'
+    assert dll.worst_case_search_time_complexity == 'n'
+    assert dll.worst_case_search_description == 'item not present'
+
+    assert dll.best_case_delete_time_complexity == '1'
+    assert dll.best_case_delete_description == 'item in head or index -1'
+    assert dll.average_case_delete_time_complexity == 'n'
+    assert dll.worst_case_delete_time_complexity == 'n'
+    assert dll.worst_case_delete_description == 'item at tail if index not provided, otherwise item in the middle'
