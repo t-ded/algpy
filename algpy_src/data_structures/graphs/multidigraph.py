6ef616f2d47bae43804e88437951c9aa8bbf108f
Tomáš Děd <tomas-ded@seznam.cz>
2024-07-21 18:03:14 +0200

    fix multidigraph edge manipulation

--- algpy_src/data_structures/graphs/multidigraph.py
+++ algpy_src/data_structures/graphs/multidigraph.py
@@ -14,7 +14,7 @@ class MultiDiGraph(BaseGraph[Node, MultiEdgeData]):
     Adjacency list representation is assumed for simplicity.
     """
 
-    def __init__(self, adjacency_list: Optional[dict[Node, dict[Node, MultiEdgeData]]]) -> None:
+    def __init__(self, adjacency_list: Optional[dict[Node, dict[Node, MultiEdgeData]]] = None) -> None:
         """
         Constructor of the MultiDiGraph class.
 
@@ -42,6 +42,7 @@ class MultiDiGraph(BaseGraph[Node, MultiEdgeData]):
         """
         Add a single edge to the graph.
         An edge is a tuple of source node, destination node and edge data.
+        Note that SingleEdgeData is still expected as data entry type.
         If an edge already exists between the two nodes, new edge's data is added to the multi edge data of the present edge.
         Both nodes are silently added to the graph in case they are not present in the graph.
 
@@ -52,9 +53,8 @@ class MultiDiGraph(BaseGraph[Node, MultiEdgeData]):
         """
         u, v, data = edge
         self.add_nodes_from({u, v})
-        if not isinstance(data, set):
-            data = {data}
-        self._adjacency_list[u].setdefault(v, set()).update(data)
+        current_data: MultiEdgeData = self._adjacency_list[u].get(v, set())
+        self._adjacency_list[u][v] = current_data.union({data})
         self.edges.add(edge)
 
     @affects_adjacency_matrix
@@ -77,10 +77,13 @@ class MultiDiGraph(BaseGraph[Node, MultiEdgeData]):
         if source not in self.nodes or target not in self.nodes:
             return
         present_data: MultiEdgeData | NoEdge = self._adjacency_list[source].get(target, NoEdge())
-        if present_data != NoEdge:
+        if not isinstance(present_data, NoEdge):
             if not data:
                 del self._adjacency_list[source][target]
-                self.edges.remove((source, target, present_data))
+                for single_edge_data in present_data:
+                    self.edges.remove((source, target, single_edge_data))
             else:
-                for value in data:
-                    self._adjacency_list[source][target].remove(value)
+                for single_edge_data in present_data:
+                    if single_edge_data in data:
+                        self.edges.remove((source, target, single_edge_data))
+                self._adjacency_list[source][target] -= set(data)
