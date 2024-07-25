cc4776920879098f2f0c49d02085bee09a06c40a
Tomáš Děd <tomas-ded@seznam.cz>
2024-07-25 12:04:35 +0200

    add increasing input size sequence generation for bfs

--- algpy_src/tests/tools/algorithm_input_generation/test_generate_increasing_input_size_sequence.py
+++ algpy_src/tests/tools/algorithm_input_generation/test_generate_increasing_input_size_sequence.py
@@ -3,9 +3,10 @@ from typing import Iterable
 import pytest
 
 from algpy_src.algorithms.algorithm import Algorithm
+from algpy_src.algorithms.graph_algorithms.traversal.bfs import BreadthFirstSearch
 from algpy_src.algorithms.sorting.bubble_sort import BubbleSort
 from algpy_src.algorithms.sorting.insertion_sort import InsertionSort
-from algpy_src.base.constants import InputSize
+from algpy_src.base.constants import InputSize, GraphSize
 from algpy_src.tests.test_utils.example_base_objects import ExampleAlgorithm
 from algpy_src.tools.algorithm_input_generation.generate_increasing_input_size_sequence import generate_increasing_input_size_sequence
 
@@ -15,8 +16,14 @@ from algpy_src.tools.algorithm_input_generation.generate_increasing_input_size_s
     [
         pytest.param(BubbleSort(), 100_000, 2, [1, 100_000], id='Bubble sort generate increasing input size sequence with 2 elements'),
         pytest.param(BubbleSort(), 10, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], id='Bubble sort generate increasing input size sequence with 10 elements'),
-        pytest.param(InsertionSort(), 100_000, 2, [1, 100_000], id='Insertion sort generate increasing input size sequence with 10 elements'),
-        pytest.param(InsertionSort(), 10, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], id='Bubble sort generate increasing input size sequence with 10 elements'),
+        pytest.param(InsertionSort(), 100_000, 2, [1, 100_000], id='Insertion sort generate increasing input size sequence with 2 elements'),
+        pytest.param(InsertionSort(), 10, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], id='Insertion sort generate increasing input size sequence with 10 elements'),
+        pytest.param(BreadthFirstSearch(), GraphSize(*(10, 10)), 10, [(i, i) for i in range(1, 11)],
+                     id='Breadth First Search generate increasing input size sequence with up to 10 nodes and edges with same counts'),
+        pytest.param(BreadthFirstSearch(), GraphSize(*(100, 10)), 10, [(10 * (i - 1) + i, i) for i in range(1, 11)],
+                     id='Breadth First Search generate increasing input size sequence with up to 10 nodes and edges with more nodes'),
+        pytest.param(BreadthFirstSearch(), GraphSize(*(10, 100)), 10, [(i, 10 * (i - 1) + i) for i in range(1, 11)],
+                     id='Breadth First Search generate increasing input size sequence with up to 10 nodes and edges with more edges'),
     ]
 )
 def test_generate_valid_increasing_input_size_sequence(algorithm: Algorithm, max_input_size: InputSize, sequence_length: int, expected_sequence: Iterable[InputSize]) -> None:
--- algpy_src/tools/algorithm_input_generation/generate_increasing_input_size_sequence.py
+++ algpy_src/tools/algorithm_input_generation/generate_increasing_input_size_sequence.py
@@ -1,10 +1,11 @@
-from typing import Iterable, TypeVar
+from typing import Iterable, TypeVar, cast
 
 import numpy as np
 
 from algpy_src.algorithms.algorithm import Algorithm
+from algpy_src.algorithms.graph_algorithms.traversal.bfs import BreadthFirstSearch
 from algpy_src.algorithms.sorting.sorting_algorithm import SortingAlgorithm
-from algpy_src.base.constants import InputSize
+from algpy_src.base.constants import InputSize, GraphSize
 
 A = TypeVar('A', bound=Algorithm)
 
@@ -31,5 +32,13 @@ def generate_increasing_input_size_sequence(algorithm: A, max_input_size: InputS
         if isinstance(max_input_size, int):
             return list(np.linspace(1, max_input_size, num=sequence_length, dtype=int))
         raise ValueError("max_input_size must be an integer for SortingAlgorithm's increasing input size sequence generation")
+
+    if isinstance(algorithm, BreadthFirstSearch):
+        if isinstance(max_input_size, GraphSize):
+            n_nodes = np.linspace(1, max_input_size.nodes, num=sequence_length, dtype=int)
+            n_edges = np.linspace(1, max_input_size.edges, num=sequence_length, dtype=int)
+            input_sizes: list[InputSize] = cast(list[InputSize], [GraphSize(*(nodes, edges)) for nodes, edges in zip(n_nodes, n_edges)])
+            return input_sizes
+        raise ValueError("max_input_size must be of GraphSize type (i.e., pair of integers) for traversal algorithm's increasing input size sequence generation")
 
     raise ValueError('No increasing input size sequence generator assigned for this algorithm class.')
