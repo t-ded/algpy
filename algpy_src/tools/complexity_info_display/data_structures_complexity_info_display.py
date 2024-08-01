b5cc58cf12bc3536ee290e5a1075dafa4d58e136
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-01 08:41:17 +0200

    add basegraph to time complexity info printing

--- algpy_src/tools/complexity_info_display/data_structures_complexity_info_display.py
+++ algpy_src/tools/complexity_info_display/data_structures_complexity_info_display.py
@@ -1,5 +1,6 @@
 from algpy_src.data_structures.container import Container
 from algpy_src.data_structures.data_structure import DataStructure
+from algpy_src.data_structures.graphs.base_graph import BaseGraph
 
 
 def print_time_complexity_info_container(container: Container) -> None:
@@ -21,6 +22,17 @@ def print_time_complexity_info_container(container: Container) -> None:
               f'{getattr(container, f"best_case_{operation}_description")}.')
 
 
+def print_time_complexity_info_graph() -> None:
+    """
+    Print time complexity information for operations of an adjacency list-based graph.
+    """
+    print('  Worst case time complexity of edge addition in adjacency list-based graph is O(1).')
+    print('  Worst case time complexity of edge removal in adjacency list-based graph is O(1).')
+    print('  Worst case time complexity of node addition in adjacency list-based graph is O(1).')
+    print('  Worst case time complexity of node removal in adjacency list-based graph is O(|V| + |E|).')
+    print('  Worst case time complexity of building an adjacency matrix in adjacency list-based graph is O(|V|^2).')
+
+
 def print_time_complexity_info_data_structure(data_structure: DataStructure) -> None:
     """
     Print the time complexity information of the given data structure.
@@ -32,3 +44,5 @@ def print_time_complexity_info_data_structure(data_structure: DataStructure) -> 
     """
     if isinstance(data_structure, Container):
         print_time_complexity_info_container(data_structure)
+    if isinstance(data_structure, BaseGraph):
+        print_time_complexity_info_graph()
