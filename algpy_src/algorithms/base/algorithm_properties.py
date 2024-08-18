ce4d15ad4b63a56b8205a74a9a49f5da6d8eb7b1
Tomáš Děd <tomas-ded@seznam.cz>
2024-08-18 09:25:20 +0200

    set up basis for refactor of algorithm properties

---
+++ algpy_src/algorithms/base/algorithm_properties.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from enum import StrEnum
+
+
+class AlgorithmFamily(StrEnum):
+
+    # Base algorithms, Test algorithms, Example algorithms
+    BASE_CLASS = 'Base Generic Algorithms'
+
+    # Container-based
+    SORTING = 'Sorting Algorithms',
+
+    # Graph-based
+    GRAPH_TRAVERSAL = 'Graph Traversal Algorithms',
+    MESSAGE_PASSING = 'Relational Classification Algorithms',
+
+    # System design
+    LOAD_BALANCING = 'Load Balancing Algorithms',
+
+
+@dataclass
+class AlgorithmProperties:
+    name: str
+    algorithm_family: AlgorithmFamily
+    is_deterministic: bool
+    best_case_time_complexity: str
+    best_case_description: str
+    average_case_time_complexity: str
+    worst_case_time_complexity: str
+    worst_case_description: str
+    space_complexity: str
+
+
+@dataclass
+class SortingAlgorithmProperties(AlgorithmProperties):
+    is_stable: bool
+
