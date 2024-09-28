0a6a529524feda768e878e0b590a1522b33762b2
Tomáš Děd <tomas-ded@seznam.cz>
2024-09-28 12:55:01 +0200

    set up fib task as basic non optimization dp task

--- algpy_src/data_structures/dynamic_programming_tasks/fibonacci_task.py
+++ algpy_src/data_structures/dynamic_programming_tasks/fibonacci_task.py
@@ -1,13 +1,25 @@
+from typing import Callable, Iterable
+
 from algpy_src.data_structures.dynamic_programming_tasks.generic_dynamic_programming_task import GenericDynamicProgrammingTask
 
 
-class NThFibonacciNumber(GenericDynamicProgrammingTask[int, int]):
+class NThFibonacciNumber(GenericDynamicProgrammingTask[int]):
 
     def __init__(self, n: int) -> None:
         super().__init__()
-        self._n = n
+        self._state: int = n
 
-    def get_state_value(self, state: int) -> int:
-        if state <= 1:
-            return 1
-        return self.get_state_value(state - 1) + self.get_state_value(state - 2)
+    def value_computation(self) -> tuple[Iterable[int], Callable]:
+        if self._state <= 2:
+            return [], lambda: 1
+        return [self._state - 1, self._state - 2], lambda a, b: a + b
+
+    def compute_value(self, value_1: int, value_2: int) -> int:
+        return value_1 + value_2
+
+    def get_state_transitions(self) -> None:
+        return None
+
+    @property
+    def is_optimization(self) -> bool:
+        return False
