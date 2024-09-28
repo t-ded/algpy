d3b5a5c5fe9af418f593c3920fe8287fe0385c05
Tomáš Děd <tomas-ded@seznam.cz>
2024-09-28 13:36:16 +0200

    add zero one knapsack problem as dp task

---
+++ algpy_src/data_structures/dynamic_programming_tasks/zero_one_knapsack_task.py
@@ -0,0 +1,32 @@
+from collections import namedtuple
+from typing import Callable, Iterable
+
+from algpy_src.data_structures.dynamic_programming_tasks.generic_dynamic_programming_task import GenericDynamicProgrammingTask
+
+ZeroOneKnapsackTaskState = namedtuple('ZeroOneKnapsackTaskState', 'taken_capacity value idx', defaults=[0, 0, 0])
+
+
+class ZeroOneKnapsackTask(GenericDynamicProgrammingTask[ZeroOneKnapsackTaskState]):
+
+    def __init__(self, capacity: int, weights: list[int], values: list[int]) -> None:
+        super().__init__()
+        self._capacity = capacity
+        self._weights = weights
+        self._values = values
+        self._state = ZeroOneKnapsackTaskState(0, 0, 0)
+
+    def value_computation(self) -> tuple[Iterable[ZeroOneKnapsackTaskState], Callable]:
+        return [], lambda: self._state.value
+
+    def get_state_transitions(self) -> list[ZeroOneKnapsackTaskState]:
+        admissible_states: list[ZeroOneKnapsackTaskState] = []
+        for i in range(self._state.idx, len(self._weights)):
+            taken_capacity_with_item = self._state.taken_capacity + self._weights[i]
+            if taken_capacity_with_item <= self._capacity:
+                admissible_states.append(ZeroOneKnapsackTaskState(taken_capacity_with_item, self._state.value + self._values[i], i + 1))
+            admissible_states.append(ZeroOneKnapsackTaskState(self._state.taken_capacity, self._state.value, i + 1))
+        return admissible_states
+
+    @property
+    def is_optimization(self) -> bool:
+        return True
