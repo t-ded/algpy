7fe999b19b0d1f6553bb52cd65e12b4eab0e240a
Tomáš Děd <tomas-ded@seznam.cz>
2024-09-29 10:25:24 +0200

    generalize type annotations for tree node

--- algpy_src/data_structures/graphs/trees/tree_node.py
+++ algpy_src/data_structures/graphs/trees/tree_node.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
-from typing import Generic, TypeVar, Optional
+from typing import Generic, TypeVar, Optional, Self
 
 _K = TypeVar('_K')
 
 
 class TreeNode(Generic[_K]):
 
-    def __init__(self, key: _K, parent: Optional[TreeNode[_K]] = None) -> None:
+    def __init__(self, key: _K, parent: Optional[Self] = None) -> None:
         """
         Constructor for the TreeNode class.
 
@@ -15,18 +15,18 @@ class TreeNode(Generic[_K]):
         ----------
         key : _K
             Key to represent the node.
-        parent : Optional[TreeNode[_K]] (default: None)
+        parent : Optional[Self] (default: None)
             Parent of this node within the tree. If not given, this node will be root-like.
         """
         self._key = key
-        self._children: list[TreeNode[_K]] = []
+        self._children: list[Self] = []
         self._depth = 0
-        self._parent: Optional[TreeNode[_K]] = None
+        self._parent: Optional[Self] = None
         if parent is not None:
             parent.add_child(self)
 
     def __eq__(self, other: object) -> bool:
-        return isinstance(other, TreeNode) and self._key == other._key
+        return isinstance(other, TreeNode) and self.key == other.key
 
     @property
     def key(self) -> _K:
@@ -41,15 +41,15 @@ class TreeNode(Generic[_K]):
         return self._depth
 
     @property
-    def parent(self) -> Optional[TreeNode[_K]]:
+    def parent(self) -> Optional[Self]:
         return self._parent
 
     @property
-    def children(self) -> list[TreeNode[_K]]:
+    def children(self) -> list[Self]:
         return self._children
 
     @property
-    def leftmost_child(self) -> Optional[TreeNode[_K]]:
+    def leftmost_child(self) -> Optional[Self]:
         return self._children[0] if len(self._children) > 0 else None
 
     def __repr__(self) -> str:
@@ -65,18 +65,18 @@ class TreeNode(Generic[_K]):
     def change_key(self, new_key: _K) -> None:
         self._key = new_key
 
-    def add_child(self, child: TreeNode[_K]) -> None:
+    def add_child(self, child: Self) -> None:
         self._children.append(child)
         child.change_parent(self)
 
-    def change_parent(self, new_parent: Optional[TreeNode[_K]]) -> None:
+    def change_parent(self, new_parent: Optional[Self]) -> None:
         self._parent = new_parent
-        self._depth = new_parent.depth + 1 if new_parent is not None else 0
+        self._depth = self._parent.depth + 1 if self._parent is not None else 0
 
     def remove_parent(self) -> None:
         self.change_parent(None)
 
-    def remove_child(self, child: TreeNode[_K]) -> None:
+    def remove_child(self, child: Self) -> None:
         self._children.remove(child)
         child.remove_parent()
 
