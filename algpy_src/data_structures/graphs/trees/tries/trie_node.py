57d2b4d0eedceffb1cfb8cb8f11c7293668bcf9d
Tomáš Děd <tomas-ded@seznam.cz>
2024-09-30 08:39:16 +0200

    generalize trie node

--- algpy_src/data_structures/graphs/trees/tries/trie_node.py
+++ algpy_src/data_structures/graphs/trees/tries/trie_node.py
@@ -1,11 +1,15 @@
 from typing import Self, Optional
 
+from typing_extensions import TypeVar
+
 from algpy_src.data_structures.graphs.trees.tree_node import TreeNode
 
+T = TypeVar('T')
 
-class TrieNode(TreeNode[str]):
 
-    def __init__(self, key: str, is_marked: bool = False, parent: Optional[Self] = None) -> None:
+class TrieNode(TreeNode[T]):
+
+    def __init__(self, key: T, is_marked: bool = False, parent: Optional[Self] = None) -> None:
         super().__init__(key, parent)
         self._is_marked = is_marked
 
